2886f72865fb9949a1d8b7e0406bc0dd4aafa228
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-06-22 08:17:44 +0900

    Update ApexLikePing.scala

--- src/main/scala/net/st915/apexlikeping/ApexLikePing.scala
+++ src/main/scala/net/st915/apexlikeping/ApexLikePing.scala
@@ -1,1 +1,46 @@
 package net.st915.apexlikeping
+
+import cats.effect.IO
+import org.bukkit.plugin.java.JavaPlugin
+
+object ApexLikePing {
+
+  private var instance: ApexLikePing = _
+
+  def getInstance: IO[ApexLikePing] = IO.pure(instance)
+
+}
+
+final class ApexLikePing extends JavaPlugin {
+
+  import cats.effect.unsafe.implicits.global
+
+  ApexLikePing.instance = this
+
+  // region startup tasks
+
+  private val startPlugin = for {
+    _ <- IO {
+      getLogger.info("ApexLikePing in enabled.")
+    }
+  }
+
+  // endregion
+
+  // region shutdown tasks
+
+  private val shutdownPlugin = for {
+    _ <- IO {
+      getLogger.info("ApexLikePing is disabled.")
+    }
+  }
+
+  // endregion
+
+  override def onEnable(): Unit =
+    startPlugin.unsafeRunSync()
+
+  override def onDisable(): Unit =
+    shutdownPlugin.unsafeRunSync()
+
+}
