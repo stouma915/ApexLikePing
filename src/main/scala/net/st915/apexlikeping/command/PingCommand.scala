83bdd47c21dab3580cdc360d00f00b7bbf4634a2
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-06-30 08:50:44 +0900

    Update PingCommand.scala

--- src/main/scala/net/st915/apexlikeping/command/PingCommand.scala
+++ src/main/scala/net/st915/apexlikeping/command/PingCommand.scala
@@ -1,3 +1,23 @@
 package net.st915.apexlikeping.command
 
-final class PingCommand
+import org.bukkit.command.{Command, CommandSender, TabExecutor}
+
+final class PingCommand extends TabExecutor {
+
+  import scala.jdk.CollectionConverters._
+
+  override def onCommand(
+    sender: CommandSender,
+    command: Command,
+    alias: String,
+    args: Array[String]
+  ): Boolean = true
+
+  override def onTabComplete(
+    sender: CommandSender,
+    command: Command,
+    alias: String,
+    args: Array[String]
+  ): java.util.List[String] = List().asJava
+
+}
