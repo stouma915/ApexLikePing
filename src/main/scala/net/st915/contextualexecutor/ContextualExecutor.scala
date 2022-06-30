a1473daba8d9d795a93522fd6687ffa05e860b93
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-07-01 08:11:06 +0900

    Update ContextualExecutor.scala

--- src/main/scala/net/st915/contextualexecutor/ContextualExecutor.scala
+++ src/main/scala/net/st915/contextualexecutor/ContextualExecutor.scala
@@ -1,3 +1,43 @@
 package net.st915.contextualexecutor
 
-trait ContextualExecutor
+import cats.effect.IO
+import cats.effect.unsafe.IORuntime
+import org.bukkit.command.{Command, CommandSender, TabExecutor}
+
+trait ContextualExecutor {
+
+  def executionWith(context: CommandContext): IO[Unit]
+
+  def tabCandidatesFor(context: CommandContext): IO[List[String]] = IO.pure(Nil)
+
+  def asBukkitTabExecutor()(implicit runtime: IORuntime): TabExecutor = new TabExecutor {
+
+    import scala.jdk.CollectionConverters.*
+
+    override def onCommand(
+      sender: CommandSender,
+      command: Command,
+      label: String,
+      args: Array[String]
+    ): Boolean = {
+      val context = CommandContext(sender, command, label, args.toList)
+
+      executionWith(context).unsafeRunSync()
+
+      true
+    }
+
+    override def onTabComplete(
+      sender: CommandSender,
+      command: Command,
+      label: String,
+      args: Array[String]
+    ): java.util.List[String] = {
+      val context = CommandContext(sender, command, label, args.toList)
+
+      tabCandidatesFor(context).unsafeRunSync()
+    }.asJava
+
+  }
+
+}
