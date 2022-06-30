ceedf1158e0b7131fd396138268753635653f18b
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-07-01 08:13:21 +0900

    Update ContextualExecutor.scala

--- src/main/scala/net/st915/contextualexecutor/ContextualExecutor.scala
+++ src/main/scala/net/st915/contextualexecutor/ContextualExecutor.scala
@@ -10,7 +10,7 @@ trait ContextualExecutor {
 
   def tabCandidatesFor(context: CommandContext): IO[List[String]] = IO.pure(Nil)
 
-  def asBukkitTabExecutor()(implicit runtime: IORuntime): TabExecutor = new TabExecutor {
+  def asBukkitTabExecutor(): TabExecutor = new TabExecutor {
 
     import scala.jdk.CollectionConverters.*
 
