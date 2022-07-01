4b7947ea57f1ca868828f65b791f4d39596ea9c4
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-07-01 11:35:33 +0900

    Update ContextualExecutorBuilder.scala

--- src/main/scala/net/st915/contextualexecutor/builder/ContextualExecutorBuilder.scala
+++ src/main/scala/net/st915/contextualexecutor/builder/ContextualExecutorBuilder.scala
@@ -4,35 +4,25 @@ import cats.effect.IO
 import net.st915.contextualexecutor.{CommandContext, ContextualExecutor}
 
 case class ContextualExecutorBuilder(
-  commandLogic: Option[CommandContext => Unit],
-  tabCompleteLogic: Option[CommandContext => List[String]]
+  commandLogic: CommandContext => Unit,
+  tabCompleteLogic: CommandContext => List[String]
 ) {
 
   def execution(func: CommandContext => Unit): ContextualExecutorBuilder =
-    ContextualExecutorBuilder(Some(func), tabCompleteLogic)
+    ContextualExecutorBuilder(func, tabCompleteLogic)
 
   def tabComplete(func: CommandContext => List[String]): ContextualExecutorBuilder =
-    ContextualExecutorBuilder(commandLogic, Some(func))
+    ContextualExecutorBuilder(commandLogic, func)
 
-  def build(): ContextualExecutor = {
-    val executeLogic = commandLogic match {
-      Some(f) => f
-      None    => _ => ()
-    }
-    val tabLogic = tabCompleteLogic match {
-      Some(f) => f
-      None    => _ => Nil
-    }
-
+  def build(): ContextualExecutor =
     new ContextualExecutor {
 
       override def executionWith(context: CommandContext): IO[Unit] =
-        IO(executeLogic(context))
+        IO.pure(commandLogic(context))
 
       override def tabCandidatesFor(context: CommandContext): IO[List[String]] =
-        IO(tabLogic(context))
+        IO.pure(tabCompleteLogic(context))
 
     }
-  }
 
 }
