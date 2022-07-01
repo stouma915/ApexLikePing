316544c590f013c5ca84a2815d40b261b9076244
Sasaki Touma <62248740+stouma915@users.noreply.github.com>
2022-07-01 12:48:34 +0900

    Update PingCommand.scala

--- src/main/scala/net/st915/apexlikeping/command/PingCommand.scala
+++ src/main/scala/net/st915/apexlikeping/command/PingCommand.scala
@@ -2,17 +2,20 @@ package net.st915.apexlikeping.command
 
 import cats.effect.IO
 import net.st915.contextualexecutor.{CommandContext, ContextualExecutor}
+import net.st915.contextualexecutor.builder.ContextualExecutorBuilder
 import org.bukkit.command.TabExecutor
 
 object PingCommand {
 
   import cats.effect.unsafe.implicits.global
 
-  val executor: TabExecutor = new ContextualExecutor {
-
-    override def executionWith(context: CommandContext): IO[Unit] = IO {
-    }
-
-  }.asBukkitTabExecutor()
+  val executor: TabExecutor =
+    ContextualExecutorBuilder
+      .begin()
+      .execution { context =>
+        println("TEST")
+      }
+      .build()
+      .asBukkitTabExecutor()
 
 }
